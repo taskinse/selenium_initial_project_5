9ccb3bbc95b62db577dab02b8cee3cd344dd000b
Seyma Taskin Yildirim <taskinseyma@gmail.com>
2023-01-17 18:43:14 +0300

    adde project

--- src/TestCase3.java
+++ src/TestCase3.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import utils.Driver;
+
+import java.util.List;
 
 public class TestCase3 {
     public static void main(String[] args) {
@@ -8,6 +12,17 @@ public class TestCase3 {
 
         //Validate Automation Exercise site SUBSCRIPTION form
 
-        Driver.quitDriver();
+        driver.get("https://automationexercise.com/");
+
+       List<WebElement> elements = driver.findElements(By.cssSelector("div[class='single-widget"));
+        for (WebElement e : elements) {
+            if (driver.findElement(By.id("susbscribe_email")).isDisplayed())
+                System.out.println("email placeholder validation PASSED");
+            if (driver.findElement(By.cssSelector("i[class*='fa fa-arrow'")).isDisplayed())
+                System.out.println("arrow submit validation PASSED");
+            System.out.println(e.getText());
+
+            Driver.quitDriver();
+        }
     }
 }
